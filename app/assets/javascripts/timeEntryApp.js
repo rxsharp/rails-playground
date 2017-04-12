2bff32342b60d4b26d7d51bb7054ac14db44ecec
Rick G <lacunainc04@gmail.com>
2017-04-11 22:20:27 -0700

    Initialize Angular

---
+++ app/assets/javascripts/timeEntryApp.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    angular.module('timeApp', [
+      'templates',
+      'ngResource'
+    ])
+    .config(["$httpProvider", function($httpProvider) {
+      $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+    }])
+    .factory('TimeEntries', ['$resource',function($resource){
+      return $resource('/dev_entries.json', {},{
+      query: { method: 'GET', isArray: true },
+      create: { method: 'POST' }
+      })
+    }])
+    .controller('TimeProject', function($location, TimeEntries, $resource){
+      var vm = this;
+      vm.title = "A title to test";
+      vm.devEntries = TimeEntries.query();
+        })
+
+}());
